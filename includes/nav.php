4143708429ed4d2e8339c7cce8d8063af2424411
gsauarv <gsauarv>
2020-08-09 20:13:35 +0545

    Booking Page Added - Saurav

---
+++ includes/nav.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Rest: The Hotel For Your Comfort.</title>
+    <script src="https://kit.fontawesome.com/469fe33d6f.js" crossorigin="anonymous"></script>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins&family=Raleway&family=Roboto&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rokkitt&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> -->
+
+</head>
+
+<body>
+    <section class="landingPage">
+        <div class="navBar">
+            <div class="navLogo">
+                <a href="index.html"><img src="img/abc.png"></a>
+            </div>
+            <div class=" navLink">
+                <ul>
+                    <li><a href="#landingPage">Home</a></li>
+                    <li><a href="#infoSection">Features</a></li>
+                    <li><a href="#descSection">Gallery</a></li>
+                    <li><a href="#foodInfo">Our History</a></li>
+                    <button class="button">Book Now</button>
+                </ul>
+            </div>
+
+        </div>
+
+
+
+    </section>
---
+++ room.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title> Rest: The Hotel For Your Comfort.</title>
+  <script src="https://kit.fontawesome.com/469fe33d6f.js" crossorigin="anonymous"></script>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins&family=Raleway&family=Roboto&display=swap"
+    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Rokkitt&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Francois+One&family=Playfair+Display&family=Quicksand&family=Yeseva+One&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+
+<body>
+
+
+  <?php 
+    include 'includes/nav.php';
+?>
+
+  <section class="rooms">
+    <!-- <h3>Room Listing</h3> -->
+    <div class="roomContainer">
+
+      <div class="roomImg">
+        <img src="img/bed2.jpg">
+      </div>
+
+      <div class="roomDesc">
+        <h3>Simple Room With Great View</h3>
+        <br><br>
+        <span>$150/Days</span>
+        <br><br><br>
+        <p>We Provide You the Best Room For You in Affordable Price. Unlike Others, we do not reduce the services
+          with respective to our prices.This Particular Room is the Cheapest Option of The Rest.
+          <br><br><br>
+          You can Spend the Great Time with the best natural view while enjoying the Best Organic Food. By Booking this
+          room you can have the great Experience of all time.
+        </p>
+        <br><br>
+        <button>View Details</button>
+      </div>
+
+
+    </div>
+
+    <div class="roomContainer">
+      <div class="roomDesc">
+        <h3>Modern Room with Happiness Included</h3>
+        <br><br>
+        <span>$250/Days</span>
+        <br><br><br>
+        <p>We Provide You the Best Room For You in Affordable Price. Unlike Others, we do not reduce the services
+          with respective to our prices.This Particular Room is the Cheapest Option of The Rest.
+          <br><br><br>
+          You can Spend the Great Time with the best natural view while enjoying the Best Organic Food. By Booking this
+          room you can have the great Experience of all time.
+        </p>
+        <br><br>
+        <button>View Details</button>
+      </div>
+
+      <div class="roomImg">
+        <img src="img/bed1.jpg">
+      </div>
+
+
+
+
+    </div>
+
+    <div class="roomContainer">
+
+      <div class="roomImg">
+        <img src="img/bed4.jpg">
+      </div>
+      <div class="roomDesc">
+        <h3>Luxury Room with Included Royalness</h3>
+        <br><br>
+        <span>$450/Days</span>
+        <br><br><br>
+        <p>We Provide You the Best Room For You in Affordable Price. Unlike Others, we do not reduce the services
+          with respective to our prices.This Particular Room is the Cheapest Option of The Rest.
+          <br><br><br>
+          You can Spend the Great Time with the best natural view while enjoying the Best Organic Food. By Booking this
+          room you can have the great Experience of all time.
+        </p>
+        <br><br>
+        <button>View Details</button>
+      </div>
+
+
+    </div>
+  </section>
+
+
+</body>
+
+</html>
