2899a6e55a39e0da1641a95effa7ff426be3fd26
gsauarv <gsauarv>
2020-08-30 18:27:08 +0545

    Modificaiton In UserDashboard

--- userDashboard.php
+++ userDashboard.php
@@ -47,7 +47,6 @@
             <ul>
                 <li><a href="userDashboard.php" class="active"><i class="fas fa-eye"></i>Overview</a></li>
                 <li><a href="BookingDetails.php"><i class="fas fa-calendar-week"></i>Booking Details</a></li>
-                <li><a href="#"><i class="fas fa-cog"></i>Services</a></li>
                 <li><a href="#"><i class="fas fa-file-invoice"></i>Bill Amounts</a></li>
                 <li><a href="#"><i class="fas fa-pen"></i>Customer Review</a></li>
             </ul>
